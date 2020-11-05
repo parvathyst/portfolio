2e6a9f1b854cf1d1b2575e6e0357506532c91c64
Parvathy S Thampi <72922758+parvathyst@users.noreply.github.com>
2020-11-05 23:36:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+function active1()
+{
+    var act,in1,in2;
+    act=document.getElementById('home');
+    in1=document.getElementById('about');
+    in2=document.getElementById('contact');
+    act.style.color="#ffffff";
+    in1.style.color="#cfb082";
+    in2.style.color="#cfb082";
+}
+
+function active2()
+{
+    var act,in1,in2;
+    act=document.getElementById('about');
+    in1=document.getElementById('home');
+    in2=document.getElementById('contact');
+    act.style.color="#ffffff";
+    in1.style.color="#cfb082";
+    in2.style.color="#cfb082";
+}
+
+function active3()
+{
+    var act,in1,in2;
+    act=document.getElementById('contact');
+    in1=document.getElementById('about');
+    in2=document.getElementById('home');
+    act.style.color="#ffffff";
+    in1.style.color="#cfb082";
+    in2.style.color="#cfb082";
+}
